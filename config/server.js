b8502b629a074d49a35fb3d0374d41d1cba0db2a
navas <gabrielnavas@email.com>
2020-11-26 02:13:21 -0300

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd2a6b370356b019999ccc65b62bb4af4'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'df191f19-5234-4d53-9e7a-aad6ec5daa3e'
+};
